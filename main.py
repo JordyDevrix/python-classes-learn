bda650e681489fdef513bb7f7701ebfb2ac82661
JordyDevrix <2109jordy2241@gmail.com>
2024-02-07 10:06:43 +0100

    Added additional functionality to car.py

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from reken import Reken
 
 
 def main():
-    car_one = Car("Mercedes", "GLS", 2023, "black")
-    print(car_one.color)
-    car_one.drive()
+    car_one = Car("Mercedes", "GLS")
+    car_two = Car("Mercedes", "GLC")
+    print(car_two.get_car_data())
+    car_two.add_values(2023, "White")
+    print(car_two.get_car_data())
+
+    #######################
+
     waardes = Reken(2, 8)
-    print(waardes.substract(reverse=True))
+    print(waardes.multiply())
 
 
 if __name__ == '__main__':
