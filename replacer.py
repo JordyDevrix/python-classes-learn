bb9cf524eb40eefc9f91f4c25359bcc2a4d3add5
JordyDevrix <2109jordy2241@gmail.com>
2024-02-16 00:57:23 +0100

    tested with strings and remade a builtin function

---
+++ replacer.py
@@ -0,0 +1,16 @@
+class String:
+
+    def __init__(self, string):
+        self.string = string
+
+    def herzet(self, oud, nieuw):
+
+        new_string = ""
+
+        for character in self.string:
+            if character == oud:
+                new_string += nieuw
+            else:
+                new_string += character
+
+        return new_string
