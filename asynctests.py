e602950bbe0b5141594dee442155ca25aaa7b29b
Jordy Devrix <2109jordy2241@gmail.com>
2024-02-18 20:57:37 +0100

    did more testing with async

--- asynctests.py
+++ asynctests.py
@@ -1,20 +1,22 @@
 import asyncio
+import time
 
 
 async def fn():
-    print("one")
+    new_task = asyncio.create_task(ook())
+    print('1')
     await asyncio.sleep(1)
-    await ook()
-    await ook()
-    print('four')
-    await asyncio.sleep(1)
-    print('five')
+    time.sleep(1)
+    print('3')
     await asyncio.sleep(1)
 
 
 async def ook():
-    val: str = input("enter: ")
-    print(val)
-
+    time.sleep(1)
+    print('2')
+    await asyncio.sleep(1)
+    time.sleep(1)
+    print('4')
+    time.sleep(1)
 
 asyncio.run(fn())
