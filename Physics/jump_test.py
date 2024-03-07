3a361b3df3d6c2a98fbe83cdb28fe1d47373dc7d
JordyDevrix <2109jordy2241@gmail.com>
2024-03-07 14:37:32 +0100

    added new functions

--- Physics/jump_test.py
+++ Physics/jump_test.py
@@ -1,15 +1,25 @@
 from pynput import keyboard
-starting_pos = 4.0
+starting_pos = 2.0
 
-def math_function():
 
-    return
+def math_function(x):
+    y = x**2
+    return y
 
 
 def on_key_release(key):
     if key == key.space:
-        print(key)
+        print(math_function(starting_pos))
+    elif key == 'q':
+        exit(0)
 
 
-with keyboard.Listener(on_release=on_key_release) as listener:
-    listener.join()
+def main():
+    with keyboard.Listener(on_release=on_key_release) as listener:
+        listener.join()
+
+
+if __name__ == '__main__':
+    main()
+
+
